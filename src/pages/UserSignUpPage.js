6c599ebf05f77a2af190bf08fe9dc16ede71fafd
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-11-10 20:00:21 -0500

    Added user

--- src/pages/UserSignUpPage.js
+++ src/pages/UserSignUpPage.js
@@ -19,9 +19,15 @@ class UserSignUpPage extends React.Component{
         const { name, value} = event.target
         const errors = { ...this.state.errors};
         errors[name] = undefined;
-        // Shorthand 
-        //const value = event.target.value;
-        //const nameField = event.target.name;
+
+        if (name === "password" || name === "passwordRepeat") 
+            if (name === "password" && value !==  this.state.passwordRepeat)
+                errors.passwordRepeat = 'Password mismatch'
+            else if (name === "passwordRepeat" && value != this.state.password)
+                errors.passwordRepeat = "Password mismatch";
+            else
+                errors.passwordRepeat = undefined;
+        
         this.setState({
             [name]: value,
             errors
@@ -31,13 +37,12 @@ class UserSignUpPage extends React.Component{
     onClickSignUp = async event =>{
         event.preventDefault();//We block browser
 
-        const { username, displayName, password, passwordRepeat} = this.state;
+        const { username, displayName, password} = this.state;
 
         const body = {
             username,
             displayName,
             password,
-            passwordRepeat
         };
 
         this.setState({pendingApiCall: true});
@@ -50,16 +55,10 @@ class UserSignUpPage extends React.Component{
         }
         this.setState({pendingApiCall: false});
     }
-        // We used then and catch because we need to wait for asyn response than wait for the result
-        // signup(body).then(responce =>{
-        //     this.setState({pendingApiCall: false});
-        // }).catch(error => {
-        //     this.setState({pendingApiCall: false});
-        // });
 
     render(){
         const { pendingApiCall, errors } = this.state;// we dont want to use this.state.pendingApiCall everytime
-        const { username, displayName, password} = errors;
+        const { username, displayName, password, passwordRepeat} = errors;
         return(
         <div className = "container">
             <form className="needs-validation">
@@ -67,13 +66,10 @@ class UserSignUpPage extends React.Component{
                 <Input name="username" label="Username" error={username} onChange={this.onChange}/>
                 <Input name="displayName" label="Display Name" error={displayName} onChange={this.onChange}/>
                 <Input name="password" label="Password" error={password} onChange={this.onChange} type="password"/>
-                <div className="form-group">
-                    <label className="form-label">Password Repeat</label>
-                    <input name="passwordRepeat" className="form-control" type="password" onChange={this.onChange}/>
-                </div>
+                <Input name="passwordRepeat" label="Password Repeat" error={passwordRepeat} onChange={this.onChange} type="password"/>
                 <div className="spacer5"></div>
                 <div className="text-center">
-                    <button className="btn btn-secondary" onClick={this.onClickSignUp} disabled={pendingApiCall}>
+                    <button className="btn btn-secondary" onClick={this.onClickSignUp} disabled={pendingApiCall || passwordRepeat !== undefined}>
                     {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>} Sign Up 
                     </button>
                 </div>
@@ -81,33 +77,6 @@ class UserSignUpPage extends React.Component{
         </div>
         );
     }
-    //this named conditional rendering this.state.pendingApiCall && <span className="spinner-border spinner-border-sm"></span>
-
-    // We dont want to repeat our code 
-    // onChangeUsername = event => {
-    //     this.setState({
-    //         username: event.target.value
-    //     });
-    // };
-
-    // onChangeDisplayName = event => {
-    //     this.setState({
-    //         displayname: event.target.value
-    //     });
-    // };
-    
-    // onChangePassword = event => {
-    //     this.setState({
-    //         password: event.target.value
-    //     });
-    // };
-
-    // onChangePasswordRepeat = event => {
-    //     this.setState({
-    //         passwordRepeat: event.target.value
-    //     });
-    // };
-
 }
 
 export default UserSignUpPage;
