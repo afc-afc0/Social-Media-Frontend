cee3f17f3bf16751f7bbce54b68b008ae4a8c9cb
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-11-13 22:27:19 -0500

    add error handling for login

--- src/pages/UserLoginPage.js
+++ src/pages/UserLoginPage.js
@@ -7,39 +7,52 @@ class LoginPage extends React.Component {
     state = {
         username: null,
         password: null,
-        errors:{
-
-        }
+        error: null
     };
 
 
     onChange = event => {
         const {name, value} = event.target;
         this.setState({
-            [name]:value
+            [name]:value,
+            error:null
         })
     }
 
-    onClickLogin = event => {
+    onClickLogin = async event => {
         event.preventDefault();
         const {username, password} = this.state
         const creds = {
             username: username,
             password: password
         }
-        login(creds);
+        this.setState({
+            error:null
+        })
+        try {
+            await login(creds);
+        } catch(apiError){
+          this.setState({
+             error: apiError.response.data.message
+          })
+        }
     }
 
     render(){
+        const {username, password, error} = this.state;
+        const buttonEnabled = username && password;
         return(
             <div className="container">
                 <form className="needs-validation">
                     <h1 className="text-center">Login</h1>
                     <Input name="username" label="Username" onChange={this.onChange} />
                     <Input name="password" label="Password" onChange={this.onChange} type="password"/>
+                    {error && <div className="alert alert-danger">{error}</div>}
                     <div className="spacer5"></div>
                     <div className="text-center">
-                        <button className="btn btn-secondary" onClick={this.onClickLogin}>Login</button>
+                        <button className="btn btn-secondary" onClick={this.onClickLogin} disabled={!buttonEnabled}>
+                            Login
+                        </button>
                     </div>
                 </form>
             </div>
