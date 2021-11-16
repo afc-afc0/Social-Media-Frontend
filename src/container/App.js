98cd16473e9cf4a2f6318902b77d8f86ff825730
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-11-16 16:57:32 -0500

    Add Routes

--- src/container/App.js
+++ src/container/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import UserSignUpPage from "../pages/UserSignUpPage";
 import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage"
+import UserPage from "../pages/UserPage";
+import {HashRouter, Route, Navigate, Routes} from 'react-router-dom';
 
 function App() {
   return (
-  <div className="row">
-    <div className="col">
-      <UserSignUpPage />
+    <div>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<UserSignUpPage />} />
+          <Route path="/user/:username" element={<UserPage/>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </HashRouter>
     </div>
-    <div className="col">
-      <LoginPage />
-    </div>
-  </div>
   );
 }
 
