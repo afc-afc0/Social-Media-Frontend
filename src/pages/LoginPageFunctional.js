6029072fc744dc5e4be18f5f64b6acda0cc6fa3a
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-02 18:33:26 -0500

    done implemented fully functional loginpage

--- src/pages/LoginPageFunctional.js
+++ src/pages/LoginPageFunctional.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Input from "../components/input";
-import { login } from "../api/apiCalls";
-import ButtonWithProgress2 from "../components/buttonWithProgress2";
-import { useInput } from "../shared/useInput";
-import { useState, useEffect, useRef, useCallback} from 'react'
+import ButtonWithProgress from "../components/buttonWithProgress";
+import { useInput} from "../shared/useInput";
 import { useNavigate  } from 'react-router-dom';
 import {useAxios} from "../shared/useAxios";
+import {useEffect} from 'react'
 
-export const LoginPage = (props) => {
+export const LoginPage = () => {
 
-    const [values, handleChange] = useInput({username: "", password: ""});
+    const [values, handleChange] = useInput({username : "", password : ""});
+    const navigate = useNavigate();
 
-    const { response, errors, loading } = useAxios({
+    const {response, apiError, loading, apiRequestCallback} = useAxios({
         method : "POST",
         url : "/api/1.0/auth",
         auth : {username : values.username , password : values.password},
@@ -20,71 +20,34 @@ export const LoginPage = (props) => {
         },
     });
 
-    const [error, setError] = useState(undefined);
-    const isMounted = useRef(false);
-    const navigate = useNavigate();
-
-    // const sendRequest = useCallback(async () => {
-    //     // don't send again while we are sending
-    //     if (isSending) return
-    //     // update state
-    //     setIsSending(true)
-    //     // send the actual request
-    //     await axios();
-    //     // once the request is sent, update state again
-    //     if (isMounted.current) // only update if we are still mounted
-    //       setIsSending(false)
-    //   }, [isSending]) 
-
-    const handleOnChange = useCallback(event => {
-        handleChange(event);
-    },[values]);
+    useEffect(() => {
+        if (response !== undefined)
+            navigate("/", { replace: true });
+    }, [response])
 
     useEffect(() => {
-        if (errors !== undefined){
-            setError(errors.message);
+        if (apiError !== null){
+            console.log("ApiError = ",apiError);
         }
-    },[errors])
-
-    useEffect(() => {
-        return () => {
-          isMounted.current = false
-        }
-    },[])
-
-    // useEffect(() => {
-    //     if (errors !== null){
-    //         setError(errors);
-    //     }
-    // }, [isSending]);
+    }, [apiError])
 
 
-    // const onClickLogin = async event => {
-    //     event.preventDefault();
+    const onButtonClick = (event) => {
+        event.preventDefault();
+        apiRequestCallback();
+    }
 
-    //     const creds = {
-    //         username: values.username,
-    //         password: values.password
-    //     }
-        
-    //     console.log("using axios");
-    //     await axios();
-    //     //navigate("/", { replace: true });      
-    // }
-
-    // const {pendingApiCall} = this.props;
     const buttonEnabled = values.username && values.password;
-    const {pendingApiCall} = false;
 
     return(
         <div className="container">
             <form className="needs-validation">
                 <h1 className="text-center">Login</h1>
-                <Input name="username" label="Username" value={values.username} onChange={handleOnChange} />
-                <Input name="password" label="Password" value={values.password} onChange={handleOnChange} type="password"/>
-                {error && <div className="alert alert-danger">{error}</div>}
+                <Input name="username" label="Username" value={values.username} onChange={handleChange}  />
+                <Input name="password" label="Password" value={values.password} onChange={handleChange} type="password"/>
+                {apiError && <div className="alert alert-danger">{apiError.message}</div>}
                 <div className="spacer5"></div>
-                {<ButtonWithProgress2 disabled={!buttonEnabled || loading} pendingApiCall={loading} text={"Login"}/>}
+                {<ButtonWithProgress onClick={onButtonClick} disabled={!buttonEnabled || loading} pendingApiCall={loading} text={"Login"}/>}
             </form>
         </div>
     );
