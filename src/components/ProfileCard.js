b9a3a5d47e52b7a415cb95e605d575140861008b
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-07 19:16:09 -0500

    update profile card to use redux store

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import {} from 'react-router-dom'
-// import { Authentication } from '../shared/AuthenticationContext'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 const ProfileCard = () => {
 
-    // const authValues = React.useContext(Authentication);
-    // const authUsername = authValues.authState.username;
+    const state = useSelector((state) => state);
+    const authUsername = state.user.username;
 
     const { username } = useParams();
 
     let message = "We can't edit";
-    // if (authUsername === username){
-    //     message = "We can edit";
-    // }
+    if (authUsername === username){
+        message = "We can edit";
+    }
 
     return (
         <div>
