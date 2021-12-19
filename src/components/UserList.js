2e5bf41ba5ac43756642b94627d5e03aa87345f3
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-19 17:05:25 -0500

    Add UserList error handling with Api progress UI

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import ButtonWithProgress from './buttonWithProgress';
 import UserListItem from './UserListItem';
 import { getUsers } from '../api/apiCalls';
 import { useApiProgress } from '../shared/useApiProgress';
@@ -7,6 +6,9 @@ import { useApiProgress } from '../shared/useApiProgress';
 export default function UserList() {
     
     const [page, setPage] = useState({content:[], size:3, number:0})
+    const pendingApiCall = useApiProgress("get", "/api/1.0/users?page");
+
+    const [loadError, setLoadError] = useState(false);
 
     useEffect(() => {
         loadUsers();
@@ -23,20 +25,44 @@ export default function UserList() {
     }
 
     const loadUsers = async (pageNum) => {
+        setLoadError(false);
         try {
             const response = await getUsers(pageNum, page.size);
             setPage(response.data);
         } catch (error) {
-            console.log(error);
+            setLoadError(true);
         }
     }
 
-    const pendingApiCall = useApiProgress("get", "/api/1.0/users?page");
     const { content: users, last, first } = page;
+
+    let actionDiv = (
+        <div>   
+            {first === false && (
+                <button className="btn btn-sm btn-secondary" onClick={onClickPrevious}>
+                    Previous
+                </button>
+            )}
+            {last === false && (
+                <button className="btn btn-sm btn-secondary float-end" onClick={onClickNext}>
+                    Next
+                </button>
+            )}
+        </div>
+    )
+
+    if (pendingApiCall) {
+        actionDiv = (
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border text-black-50">
+                    <span className="sr-only" />
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <ButtonWithProgress onClick={() => loadUsers()} disabled={pendingApiCall} pendingApiCall={pendingApiCall} text={"Get Users"}/>
             <div className="card">
                 <h3 className="card-header text-center">Users</h3>
                 <div className="list-group-flush"> 
@@ -45,18 +71,8 @@ export default function UserList() {
                     ))}
                     
                 </div>
-                <div>   
-                    {first === false && (
-                        <button className="btn btn-sm btn-secondary" onClick={onClickPrevious}>
-                            Previous
-                        </button>
-                    )}
-                    {last === false && (
-                        <button className="btn btn-sm btn-secondary float-end" onClick={onClickNext}>
-                            Next
-                        </button>
-                    )}
-                </div>
+                {actionDiv}
+                {loadError && <div className="text-center text-danger">Error while loading users</div>}
             </div>
         </div>
     )
