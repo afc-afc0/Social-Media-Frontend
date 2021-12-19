ec9ad3ef20ad4752ed55284e56f5ceb2459c9534
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-19 18:39:48 -0500

    add UserNotFound error handling to userPage

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -6,7 +6,13 @@ import { useParams } from 'react-router';
 
 const UserPage = () => {
     const [user, setUser] = useState();
+    const [notFound, setNotFound] = useState(false);
+    
     const { username } = useParams();
+
+    useEffect(() => {
+        setNotFound(false);
+    }, [user])
 
     useEffect(() => {
         const loadUser = async () => {
@@ -14,13 +20,27 @@ const UserPage = () => {
                 const response = await getUser(username);
                 setUser(response.data);
             } catch (error) {
-                console.log(error);
+                setNotFound(true);
             }
         };
 
         loadUser();
     }, [username])
 
+    if (notFound) {
+        return (
+            <div className="container">
+                <div className="alert alert-danger text-center">
+                    <div>
+                        <i className="material-icons" style={{fontSize: "48px"}}>
+                            error
+                        </i>
+                    </div>
+                    User not found
+                </div>
+            </div>
+        )
+    }
     
     return (
         <div className="container">
