9ac3fd20902fb5e558154e14c3b47a4c20069237
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-19 17:46:26 -0500

    Update UserPage add loadUser function

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
 import ProfileCard from '../components/ProfileCard';
+import { useState, useEffect } from "react";
+import { getUser } from '../api/apiCalls';
+import { useParams } from 'react-router';
 
-const UserPage = (props) => {
+const UserPage = () => {
+    const [user, setUser] = useState();
+    const { username } = useParams();
 
+    useEffect(() => {
+        const loadUser = async () => {
+            try {
+                const response = await getUser(username);
+                setUser(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        loadUser();
+    }, [username])
+
+    
     return (
         <div className="container">
-            <ProfileCard  username={props.username} />
+            <ProfileCard  username={username} />
+            <h1>{username}</h1>
         </div>
     );
 };
