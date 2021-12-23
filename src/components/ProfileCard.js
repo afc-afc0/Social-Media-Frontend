9b867bdba1a5da43a8c6894483d305b2c83d61fd
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-22 21:38:23 -0500

    Add ProfileCard editing

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,23 +1,64 @@
 import React from 'react'
-import {} from 'react-router-dom'
+import ProfileImageWithDefault from './ProfileImageWithDefault';
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { useState } from 'react'
 
-const ProfileCard = () => {
+
+const ProfileCard = (props) => {
+    const [inEditMode, setInEditMode] = useState(false);
 
     const state = useSelector((state) => state);
-    const authUsername = state.user.username;
+    const loggedInUsername = state.user.username;
 
-    const { username } = useParams();
+    const { username: pathUsername } = useParams();
+
+    const { user } = props;
+    const { username, displayName, image } = user;
 
     let message = "We can't edit";
-    if (authUsername === username){
+    if (loggedInUsername === pathUsername){
         message = "We can edit";
     }
 
     return (
-        <div>
-            {message}
+        <div className="card text-center">
+            <div className="card-header ">
+                <ProfileImageWithDefault className="rounded-circle shadow"  width="260" height="200" alt={`${username}`} image={image}></ProfileImageWithDefault>
+            </div>
+            <div className="card-body">
+                {!inEditMode && 
+                    (
+                    <div>
+                        <h3>
+                            {displayName}@{username} 
+                        </h3>
+                        <button className="btn btn-success d-inline-flex" onClick={() => setInEditMode(true)}>
+                            <span className="material-icons">
+                                edit
+                            </span>Edit
+                        </button>
+                    </div>
+                    )
+                }
+                {inEditMode && 
+                    (
+                    <div>
+                        <input label="Change Display Name"></input>
+                        <div>
+                            <button className="btn btn-secondary d-inline-flex">
+                                <span className="material-icons">save</span>
+                                Save
+                            </button>    
+                            <button className="btn btn-light d-inline-flex ml-1" onClick={() => setInEditMode(false)}>
+                                <span className="material-icons">close</span>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
