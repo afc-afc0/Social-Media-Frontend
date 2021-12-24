8be22c00cf8d4cd6761dc74e294aca3b1951d335
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-23 19:46:49 -0500

    Update Profile Card to diplay user profile

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,25 +1,58 @@
 import React from 'react'
+import Input from './input';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { updateUser } from '../api/apiCalls';
+import { useApiProgress } from '../shared/useApiProgress';
+import ButtonWithProgress from './buttonWithProgress';
 
 
 const ProfileCard = (props) => {
     const [inEditMode, setInEditMode] = useState(false);
+    const [updatedDisplayName, setUpdatedDisplayName] = useState();
+    const [user, setUser] = useState({}); 
+    const [editable, setEditable] = useState(false);
 
     const state = useSelector((state) => state);
     const loggedInUsername = state.user.username;
 
     const { username: pathUsername } = useParams();
-
-    const { user } = props;
     const { username, displayName, image } = user;
 
-    let message = "We can't edit";
-    if (loggedInUsername === pathUsername){
-        message = "We can edit";
+
+    useEffect(() => {
+        setUser(props.user);
+    }, [props.user])
+
+    useEffect(() => {
+        setEditable(pathUsername === loggedInUsername)
+    }, [pathUsername, loggedInUsername])
+
+    useEffect(() => {
+        if(!inEditMode){
+            setUpdatedDisplayName(undefined);
+        } else { 
+            setUpdatedDisplayName(displayName);
+        }
+    }, [inEditMode, displayName])
+
+    const onClickSave = async () => {
+        const body = {
+            displayName: updatedDisplayName
+        }
+
+        try {
+            const response = await updateUser(username, body);
+            setInEditMode(false);
+            setUser(response.data);
+        } catch (error) {
+
+        }
     }
+
+    const pendingApiCall = useApiProgress('put', 'api/1.0/users/' + username, true);
 
     return (
         <div className="card text-center">
@@ -33,27 +66,42 @@ const ProfileCard = (props) => {
                         <h3>
                             {displayName}@{username} 
                         </h3>
-                        <button className="btn btn-success d-inline-flex" onClick={() => setInEditMode(true)}>
-                            <span className="material-icons">
-                                edit
-                            </span>Edit
-                        </button>
+                        { editable &&
+                            <button className="btn btn-success d-inline-flex" onClick={() => setInEditMode(true)}>
+                                <span className="material-icons">
+                                    edit
+                                </span>Edit
+                            </button>
+                        }
                     </div>
                     )
                 }
                 {inEditMode && 
                     (
                     <div>
-                        <input label="Change Display Name"></input>
+                        <Input 
+                            label="Change Display Name" 
+                            defaultValue={displayName} 
+                            onChange={(event) => setUpdatedDisplayName(event.target.value)}>
+                        </Input>
                         <div>
-                            <button className="btn btn-secondary d-inline-flex">
-                                <span className="material-icons">save</span>
-                                Save
-                            </button>    
-                            <button className="btn btn-light d-inline-flex ml-1" onClick={() => setInEditMode(false)}>
+                            <ButtonWithProgress
+                                className="btn btn-secondary d-inline-flex" 
+                                onClick={onClickSave}
+                                disabled={pendingApiCall}
+                                pendingApiCall={pendingApiCall}
+                                text={
+                                        <>
+                                            <span className="material-icons">save</span>
+                                            Save
+                                        </>
+                                }   
+                            />
+                            <button className="btn btn-danger d-inline-flex ml-1" onClick={() => setInEditMode(false)} disabled={pendingApiCall}>
                                 <span className="material-icons">close</span>
                                 Cancel
                             </button>
+                            
                         </div>
                     </div>
                     )
