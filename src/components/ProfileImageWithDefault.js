9032078df3efd531d5e8071051549cd8dd220131
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-12-28 17:47:51 -0500

    Topbar updates

--- src/components/ProfileImageWithDefault.js
+++ src/components/ProfileImageWithDefault.js
@@ -2,14 +2,20 @@ import React from 'react'
 import defaultUserPicture from "../assets/defaultUserPicture.png"
 
 const ProfileImageWithDefault = (props) => {
-    const { image, tempImage } = props;
+    const { image, tempimage } = props;
 
     let imageSource = defaultUserPicture;
     if (image) {
-        imageSource = image;
+        imageSource = 'images/' + image;
     }
 
-    return <img alt={"Profile"} src={tempImage || imageSource} {...props} />          
+    return <img 
+                alt={"Profile"} 
+                src={tempimage || imageSource} {...props} 
+                onError={(event) => {
+                    event.target.src = defaultUserPicture
+                }}
+            />          
 }
 
 export default ProfileImageWithDefault;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,14 +4,36 @@ import { Link } from 'react-router-dom'
 import { useSelector, useDispatch} from 'react-redux'
 import { bindActionCreators } from 'redux'; 
 import { actionCreators } from "../redux/index";
+import { useState, useEffect, useRef } from 'react';
+import ProfileImageWithDefault from './ProfileImageWithDefault';
 
 export const TopBar = () => {
 
-    const state = useSelector((state) => state);
-    const { username, isLoggedIn } = state.user;
+    const { isLoggedIn, username, displayName, image } = useSelector((store) => ({
+        isLoggedIn : store.user.isLoggedIn,
+        username : store.user.username,
+        displayName : store.user.displayName,
+        image : store.user.image
+    }));
+    
+    const menuArea = useRef(null);
 
     const dispatch = useDispatch();
     const { userLogout } = bindActionCreators(actionCreators, dispatch);
+    const [menuVisible, setMenuVisible] = useState(false);
+
+
+    useEffect(() => {
+        document.addEventListener("click", onMenuClick);
+        return () => {
+            document.removeEventListener("click", onMenuClick);
+        }
+    }, [isLoggedIn])
+
+    const onMenuClick = (event) => {
+        if(menuArea.current === null || !menuArea.current.contains(event.target))
+            setMenuVisible(false);
+    }
 
     let links = (
         <ul className="navbar-nav ms-auto">
@@ -29,15 +51,43 @@ export const TopBar = () => {
     );
 
     if (isLoggedIn) {
+        let dropdownClass = "dropdown-menu p-0 shadow";
+        if (menuVisible) {
+            dropdownClass += " show";
+        }
+
         links = (
-        <ul className="navbar-nav ms-auto">
-            <li>
-                <Link className="nav-link" to={`/user/${username}`}>
-                    {username}
-                </Link>
-            </li>
-            <li className="nav-link" onClick={() => userLogout()} style={{cursor: 'pointer'}}>{"Logout"}</li>  
-        </ul>
+            <ul className="navbar-nav ms-auto" ref={menuArea}>
+                <li className="nav-item dropdown">
+                    <div className='d-flex' style={{ cursor: 'pointer'}} onClick={() => setMenuVisible(true)}>
+                        <ProfileImageWithDefault 
+                            className="rounded-circle m-auto" 
+                            image={image} 
+                            width="32" 
+                            height="32"
+                        />
+                        <span className='nav-link dropdown-toggle'>{displayName}</span>
+                    </div>
+                    <div className={dropdownClass}>
+                        <Link 
+                            className="dropdown-item d-flex p-2" 
+                            to={`/user/${username}`}
+                            onClick={() => setMenuVisible(false)}
+                        >
+                            <span className="material-icons text-info me-2" >account_circle</span>
+                            My Profile
+                        </Link>
+                        <span 
+                            className="dropdown-item d-flex p-2" 
+                            onClick={() => userLogout()} 
+                            style={{cursor: 'pointer'}}
+                        >
+                            <span className="material-icons text-danger me-2">logout</span>
+                            {"Logout"}
+                        </span>
+                    </div>
+                </li>       
+            </ul>
         );
     }
 
